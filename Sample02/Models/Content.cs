e2fae5be1a3ac153499f24cc460c7baf22094446
LoveKK <fkueyvi@outlook.com>
2019-11-02 14:23:16 +0800

    注释内容

---
+++ Sample02/Models/Content.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample02.Models
+{
+    /// <summary>
+    /// 内容实体
+    /// </summary>
+    public class Content
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Contenr { get; set; }
+
+        /// <summary>
+        /// 状态 1 是正常， 0 是删除
+        /// </summary>
+        public int Status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime StartTime { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime ModifyTime { get; set; }
+
+
+
+    }
+}
